4ad5f33216a8e961bcb03a89d25c209abe320130
Adam Rice <adam.arthur.rice@gmail.com>
2012-06-01 23:07:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politiscale::Application.config.secret_token = 'ad2f623a451906c2cfe794eea4689c1064fd2271668dfda92065baf52789183e6bf38fe284e640ce62dbaee48369a0c24197e2de251af1a491b6b0ded7b0c4d5'
